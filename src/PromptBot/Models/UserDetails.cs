5c9127dbcb21f07597bacbcd7a445477ce564dbc
Muhammed Rafi <rafi.genius.cs@gmail.com>
2023-08-04 11:53:40 +0400

    First commit

---
+++ src/PromptBot/Models/UserDetails.cs
@@ -0,0 +1,10 @@
+﻿namespace PromptBot.Models;
+
+public class UserDetails
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public string JoiningDate { get; set; }
+    public string Language { get; set; }
+    public string ProfilePhoto { get; set; }
+}
